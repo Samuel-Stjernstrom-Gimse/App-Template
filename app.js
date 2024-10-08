58e1cd668d349e8fc207c4893d0f91f05b0db8a2
Samuel Stjernstrøm-Gimse <153206324+Samuel-Stjernstrom-Gimse@users.noreply.github.com>
2024-10-08 13:12:43 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const getData = dataSource =>
+    new Promise((resolve, reject) =>
+        dataSource.refreshDataSource(err => err
+            ? reject(err)
+            : resolve(!err)
+        )
+    );
+
+const app = async () => {
+    try {
+        /*
+        setWhere()
+        await Promise.all([
+            getData(ds1),
+            getData(ds2), // parallel loading 
+        ]);
+        
+        useData()
+        
+        await getData(ds3) // blocking loadig
+        */
+    } catch (error) {
+        console.error('Error refreshing data sources:', error);
+    }
+};
+
+(async () => await app())();
+
+
+
+
+
+
+
